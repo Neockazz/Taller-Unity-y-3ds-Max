621cb99e5cf91f1eb756b56f54af341d3fbc3839
Neockazz <sb101010@ingenieria.sanmartin.edu.co>
2014-02-27 01:26:34 -0500

    Proyecto 3ds Max y carro creado en C# Unity

---
+++ Carro c#/Carroc#.js
@@ -0,0 +1,106 @@
+﻿#pragma strict
+
+function Start () {
+
+		var cube : GameObject  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube.transform.position = Vector3(0.1851258, 1.212802, -2.374351);
+		cube.transform.Rotate(0f,0f,0f);
+		cube.transform.localScale=Vector3(1.59038,0.5006871,2.594095);
+		cube.AddComponent("BoxCollider");
+		cube.AddComponent("MeshRenderer");
+		var cube2 : GameObject  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube2.transform.position = Vector3(0.1851258, 1.341494, -0.5406924);
+		cube2.transform.Rotate(0f,0f,0f);
+		cube2.transform.localScale=Vector3(1.59038,0.5006871,-1.219515);
+		cube2.AddComponent("BoxCollider");
+		cube2.AddComponent("MeshRenderer");
+		var cube3 : GameObject  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube3.transform.position = Vector3(0.1851258, 1.1578, -0.7438473);
+		cube3.transform.Rotate(90f,0f,0f);
+		cube3.transform.localScale=Vector3(1.59038,0.5006871,-0.4055409);
+		cube3.AddComponent("BoxCollider");
+		cube3.AddComponent("MeshRenderer");
+		var cube4 : GameObject  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube4.transform.position = Vector3(0.1851258, 2.302082, -0.6670475);
+		cube4.transform.Rotate(0f,0f,0f);
+		cube4.transform.localScale=Vector3(1.59038,0.5006871,-0.9767075);
+		cube4.AddComponent("BoxCollider");
+		cube4.AddComponent("MeshRenderer");
+		var cube5 : GameObject  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube5.transform.position = Vector3(0.862036,1.413467,-2.371165);
+		cube5.transform.Rotate(0f,0f,0f);
+		cube5.transform.localScale=Vector3(0.2406305,0.5006871,2.594095);
+		cube5.AddComponent("BoxCollider");
+		cube5.AddComponent("MeshRenderer");
+		var cube6 : GameObject  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube6.transform.position = Vector3(0.1851258,0.8510752,-0.6853967);
+		cube6.transform.Rotate(0f,0f,0f);
+		cube6.transform.localScale=Vector3(1.59038,0.5006871,3.349324);
+		cube6.AddComponent("BoxCollider");
+		cube6.AddComponent("MeshRenderer");
+		var cube7 : GameObject  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube7.transform.position = Vector3(0.1851258,1.341675,0.5007968);
+		cube7.transform.Rotate(12.13944f,0f,0f);
+		cube7.transform.localScale=Vector3(1.59038,0.5006871,-0.9767075);
+		cube7.AddComponent("BoxCollider");
+		cube7.AddComponent("MeshRenderer");
+		var cube8 : GameObject  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube8.transform.position = Vector3(0.1851258,0.8510752,-1.995752);
+		cube8.transform.Rotate(0f,0f,0f);
+		cube8.transform.localScale=Vector3(1.59038,0.5006871,3.349324);
+		cube8.AddComponent("BoxCollider");
+		cube8.AddComponent("MeshRenderer");
+		var cube9 : GameObject  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube9.transform.position = Vector3(0.1851258,2.009713,-0.2253701);
+		cube9.transform.Rotate(70.31223f,0f,0f);
+		cube9.transform.localScale=Vector3(1.59038,0.5006871,-0.9767076);
+		cube9.AddComponent("BoxCollider");
+		cube9.AddComponent("MeshRenderer");
+		var cube10 : GameObject  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube10.transform.position = Vector3(0.1851258,1.841046,-0.6670475);
+		cube10.transform.Rotate(0f,0f,0f);
+		cube10.transform.localScale=Vector3(1.59038,0.5006871,-0.9767075);
+		cube10.AddComponent("BoxCollider");
+		cube10.AddComponent("MeshRenderer");
+		var cube11 : GameObject  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube11.transform.position = Vector3(0.1699725,1.413467,-3.546809);
+		cube11.transform.Rotate(0f,90f,0f);
+		cube11.transform.localScale=Vector3(0.2406305,0.5006871,1.326656);
+		cube11.AddComponent("BoxCollider");
+		cube11.AddComponent("MeshRenderer");
+		var cube12 : GameObject  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube12.transform.position = Vector3(-0.5009881,1.413467,-2.374882);
+		cube12.transform.Rotate(0f,0f,0f);
+		cube12.transform.localScale=Vector3(0.2406305,0.5006871,2.594095);
+		cube12.AddComponent("BoxCollider");
+		cube12.AddComponent("MeshRenderer");
+		var cylinder : GameObject = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+		cylinder.transform.position = Vector3(1.081525,0.8510752,0.2259815);
+		cylinder.transform.Rotate(90f,270f,0f);
+		cylinder.transform.localScale= Vector3(1.225088,0.09770568,1.225088);
+		cylinder.AddComponent("CapsuleCollider");
+		cylinder.AddComponent("MeshRenderer");
+		var cylinder2 : GameObject = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+		cylinder2.transform.position = Vector3(-0.7125183,0.8510752,-2.906469);
+		cylinder2.transform.Rotate(90f,270f,0f);
+		cylinder2.transform.localScale= Vector3(1.225088,0.09770568,1.225088);
+		cylinder2.AddComponent("CapsuleCollider");
+		cylinder2.AddComponent("MeshRenderer");
+		var cylinder3 : GameObject = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+		cylinder3.transform.position = Vector3(1.081525,0.8510752,-2.906469);
+		cylinder3.transform.Rotate(90f,270f,0f);
+		cylinder3.transform.localScale= Vector3(1.225088,0.09770568,1.225088);
+		cylinder3.AddComponent("CapsuleCollider");
+		cylinder3.AddComponent("MeshRenderer");
+		var cylinder4 : GameObject = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+		cylinder4.transform.position = Vector3(-0.7125183,0.8510752,0.2259815);
+		cylinder4.transform.Rotate(90f,270f,0f);
+		cylinder4.transform.localScale= Vector3(1.225088,0.09770568,1.225088);
+		cylinder4.AddComponent("CapsuleCollider");
+		cylinder4.AddComponent("MeshRenderer");
+		
+}
+
+function Update () {
+
+}
---
+++ Carro c#/carroc.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class carroc : MonoBehaviour {
+
+	void Start () {
+		
+		GameObject cube1  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube1.transform.position = new Vector3(0.1851258f, 1.212802f, -2.374351f);
+		cube1.transform.Rotate(0f,0f,0f);
+		cube1.transform.localScale= new Vector3(1.59038f,0.5006871f,2.594095f);
+		cube1.AddComponent("BoxCollider");
+		cube1.AddComponent("MeshRenderer");
+		GameObject cube2  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube2.transform.position = new Vector3(0.1851258f, 1.341494f, -0.5406924f);
+		cube2.transform.Rotate(0f,0f,0f);
+		cube2.transform.localScale= new Vector3(1.59038f,0.5006871f,-1.219515f);
+		cube2.AddComponent("BoxCollider");
+		cube2.AddComponent("MeshRenderer");
+		GameObject cube3  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube3.transform.position = new Vector3(0.1851258f, 1.1578f, -0.7438473f);
+		cube3.transform.Rotate(90f,0f,0f);
+		cube3.transform.localScale= new Vector3(1.59038f,0.5006871f,-0.4055409f);
+		cube3.AddComponent("BoxCollider");
+		cube3.AddComponent("MeshRenderer");
+		GameObject cube4  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube4.transform.position = new Vector3(0.1851258f, 2.302082f, -0.6670475f);
+		cube4.transform.Rotate(0f,0f,0f);
+		cube4.transform.localScale= new Vector3(1.59038f,0.5006871f,-0.9767075f);
+		cube4.AddComponent("BoxCollider");
+		cube4.AddComponent("MeshRenderer");
+		GameObject cube5  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube5.transform.position = new Vector3(0.862036f,1.413467f,-2.371165f);
+		cube5.transform.Rotate(0f,0f,0f);
+		cube5.transform.localScale= new Vector3(0.2406305f,0.5006871f,2.594095f);
+		cube5.AddComponent("BoxCollider");
+		cube5.AddComponent("MeshRenderer");
+		GameObject cube6  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube6.transform.position = new Vector3(0.1851258f,0.8510752f,-0.6853967f);
+		cube6.transform.Rotate(0f,0f,0f);
+		cube6.transform.localScale= new Vector3(1.59038f,0.5006871f,3.349324f);
+		cube6.AddComponent("BoxCollider");
+		cube6.AddComponent("MeshRenderer");
+		GameObject cube7  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube7.transform.position = new Vector3(0.1851258f,1.341675f,0.5007968f);
+		cube7.transform.Rotate(12.13944f,0f,0f);
+		cube7.transform.localScale= new Vector3(1.59038f,0.5006871f,-0.9767075f);
+		cube7.AddComponent("BoxCollider");
+		cube7.AddComponent("MeshRenderer");
+		GameObject cube8  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube8.transform.position = new Vector3(0.1851258f,0.8510752f,-1.995752f);
+		cube8.transform.Rotate(0f,0f,0f);
+		cube8.transform.localScale= new Vector3(1.59038f,0.5006871f,3.349324f);
+		cube8.AddComponent("BoxCollider");
+		cube8.AddComponent("MeshRenderer");
+		GameObject cube9 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube9.transform.position = new Vector3(0.1851258f,2.009713f,-0.2253701f);
+		cube9.transform.Rotate(70.31223f,0f,0f);
+		cube9.transform.localScale= new Vector3(1.59038f,0.5006871f,-0.9767076f);
+		cube9.AddComponent("BoxCollider");
+		cube9.AddComponent("MeshRenderer");
+		GameObject cube10  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube10.transform.position = new Vector3(0.1851258f,1.841046f,-0.6670475f);
+		cube10.transform.Rotate(0f,0f,0f);
+		cube10.transform.localScale= new Vector3(1.59038f,0.5006871f,-0.9767075f);
+		cube10.AddComponent("BoxCollider");
+		cube10.AddComponent("MeshRenderer");
+		GameObject cube11  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube11.transform.position = new Vector3(0.1699725f,1.413467f,-3.546809f);
+		cube11.transform.Rotate(0f,90f,0f);
+		cube11.transform.localScale= new Vector3(0.2406305f,0.5006871f,1.326656f);
+		cube11.AddComponent("BoxCollider");
+		cube11.AddComponent("MeshRenderer");
+		GameObject cube12  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube12.transform.position = new Vector3(-0.5009881f,1.413467f,-2.374882f);
+		cube12.transform.Rotate(0f,0f,0f);
+		cube12.transform.localScale= new Vector3(0.2406305f,0.5006871f,2.594095f);
+		cube12.AddComponent("BoxCollider");
+		cube12.AddComponent("MeshRenderer");
+		GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+		cylinder.transform.position = new Vector3(1.081525f,0.8510752f,0.2259815f);
+		cylinder.transform.Rotate(90f,270f,0f);
+		cylinder.transform.localScale= new Vector3(1.225088f,0.09770568f,1.225088f);
+		cylinder.AddComponent("CapsuleCollider");
+		cylinder.AddComponent("MeshRenderer");
+		GameObject cylinder2 = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+		cylinder2.transform.position = new Vector3(-0.7125183f,0.8510752f,-2.906469f);
+		cylinder2.transform.Rotate(90f,270f,0f);
+		cylinder2.transform.localScale= new Vector3(1.225088f,0.09770568f,1.225088f);
+		cylinder2.AddComponent("CapsuleCollider");
+		cylinder2.AddComponent("MeshRenderer");
+		GameObject cylinder3 = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+		cylinder3.transform.position = new Vector3(1.081525f,0.8510752f,-2.906469f);
+		cylinder3.transform.Rotate(90f,270f,0f);
+		cylinder3.transform.localScale= new Vector3(1.225088f,0.09770568f,1.225088f);
+		cylinder3.AddComponent("CapsuleCollider");
+		cylinder3.AddComponent("MeshRenderer");
+		GameObject cylinder4 = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+		cylinder4.transform.position = new Vector3(-0.7125183f,0.8510752f,0.2259815f);
+		cylinder4.transform.Rotate(90f,270f,0f);
+		cylinder4.transform.localScale= new Vector3(1.225088f,0.09770568f,1.225088f);
+		cylinder4.AddComponent("CapsuleCollider");
+		cylinder4.AddComponent("MeshRenderer");
+		
+	}
+	
+	void Update () {
+		
+	}
+}
